2b6b0850559c1b21efd1ef2742a736e06bd06fe5
dopamane <dwc1295@gmail.com>
2026-08-17 08:36:04 -0700

    setup lib

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for intelhex
+
+## 0.1.0.0 -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ lib/Codec/IntelHEX.hs
@@ -0,0 +1,8 @@
+module Codec.IntelHEX (readIntelHEX) where
+
+import Data.ByteString.Builder
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as BS
+
+readIntelHEX :: ByteString -> ByteString
+readIntelHEX = undefined
