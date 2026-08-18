549c5d2b93b0555ff272601634688721cba963a9
dopamane <dwc1295@gmail.com>
2026-08-18 00:23:00 -0700

    add unit test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import Codec.IntelHEX
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = defaultMain $ testGroup "Test.IntelHEX"
+  [ helloWorldTest
+  ]
+
+helloWorldTest :: TestTree
+helloWorldTest = testCase "hello-world" $
+  readIntelHEX helloWorldHEX @?= "Hello, World\n"
+
+helloWorldHEX :: String
+helloWorldHEX = ":0D00000048656C6C6F2C20576F726C640AA1\n:00000001FF\n"
