08a5f9b7aa5955b58cca8b8b719ae55cbc1f1ee0
dopamane <dwc1295@gmail.com>
2026-08-19 04:27:07 -0700

    read after write prop

--- test/Main.hs
+++ test/Main.hs
@@ -3,13 +3,20 @@
 module Main (main) where
 
 import Codec.IntelHEX
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Lazy.Char8 as BSC
 import Data.String
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Hedgehog
 
 main :: IO ()
 main = defaultMain $ testGroup "Test.IntelHEX"
   [ helloWorldTest
+  , readAfterWriteTest
   ]
 
 helloWorldTest :: TestTree
@@ -21,3 +28,8 @@ helloWorldTest = testCaseSteps "hello-world" $ \step -> do
 
 helloWorldHEX :: String
 helloWorldHEX = ":0D00000048656C6C6F2C20576F726C640AA1\n:00000001FF\n"
+
+readAfterWriteTest :: TestTree
+readAfterWriteTest = testProperty "read-after-write" $ property $ do
+  bs <- forAll $ BS.fromStrict <$> Gen.bytes (Range.linear 0 16384)
+  (readIntelHEX . BSC.unpack . writeIntelHEX) bs === bs
